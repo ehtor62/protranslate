09681baed01c25298cf28d994ab858d31677d7a5
Martin Rothe <martin.r.rothe@gmail.com>
2026-02-03 09:47:09 +0100

    slider

--- src/components/ContextSlider.tsx
+++ src/components/ContextSlider.tsx
@@ -62,9 +62,25 @@ export function ContextSlider({
             onChange={(e) => onChange(parseInt(e.target.value))}
             step="1"
             className={cn(
-              "w-full h-2",
+              "w-full h-2 cursor-pointer touch-none",
+              "focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2",
+              "[&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-5 [&::-webkit-slider-thumb]:h-5",
+              "[&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-primary",
+              "[&::-webkit-slider-thumb]:cursor-pointer [&::-webkit-slider-thumb]:shadow-md",
+              "[&::-webkit-slider-thumb]:transition-all [&::-webkit-slider-thumb]:hover:scale-110",
+              "[&::-webkit-slider-thumb]:active:scale-125",
+              "[&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:w-5 [&::-moz-range-thumb]:h-5",
+              "[&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-primary [&::-moz-range-thumb]:border-0",
+              "[&::-moz-range-thumb]:cursor-pointer [&::-moz-range-thumb]:shadow-md",
+              "[&::-webkit-slider-runnable-track]:h-2 [&::-webkit-slider-runnable-track]:rounded-full",
+              "[&::-webkit-slider-runnable-track]:bg-muted",
+              "[&::-moz-range-track]:h-2 [&::-moz-range-track]:rounded-full [&::-moz-range-track]:bg-muted",
               showLabelInHandle && "range-with-label"
             )}
+            style={{
+              WebkitTapHighlightColor: 'transparent',
+              touchAction: 'none'
+            }}
           />
         </div>
       </div>
