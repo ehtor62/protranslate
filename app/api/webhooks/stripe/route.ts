ec47b50969423d18926761f532973466da9751d1
Martin Rothe <martin.r.rothe@gmail.com>
2026-02-04 14:00:26 +0100

    API

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -4,7 +4,7 @@ import { adminDb } from '@/lib/firebase-admin';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2026-01-28.clover',
 });
 
 const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET;
